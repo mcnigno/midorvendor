2bc7170d10a4bb6b1505884d6bbd28ca80471fe6
Mc Nigno <mcnigno@gmail.com>
2019-10-06 10:31:23 +0200

    server name

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Your App secret key
 SECRET_KEY = '\2\1thisismyscretkey\1\2\e\y\y\h'
-
+SERVER_NAME = 'midorvendor.quasarpm.com'
 # The SQLAlchemy connection string.
 #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_DATABASE_URI = 'mysql://midorv:midorv300777@localhost/midorvendor'
